918c3d8efcf33ba87a7ab74e7c71a1835c13ab39
Gerfy1 <gerfynlol@gmail.com>
2025-02-07 20:33:02 -0300

    Initial commit

---
+++ Jhizhu/src/main/java/com/jizhu/Jhizhu/JhizhuApplication.java
@@ -0,0 +1,13 @@
+package com.jizhu.Jhizhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhizhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhizhuApplication.class, args);
+	}
+
+}
